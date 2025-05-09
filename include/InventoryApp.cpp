cb57e1c6bbbbf7de33d23318369ed953935f3648
jooecoodes <jefeljoevillacorta@gmail.com>
2025-05-09 09:02:48 +0800

    Removed coms

--- include/InventoryApp.cpp
+++ include/InventoryApp.cpp
@@ -3,13 +3,11 @@
 #include <iostream>
 
 InventoryApp::InventoryApp() : inventory() {
-    // Constructor implementation
+    
 }
 
 void InventoryApp::run() {
-    // Main application logic
     std::cout << "Welcome to the Inventory Management System!" << std::endl;
-    // Additional code for running the application
     
     inventory.addProduct("1", "Product A", "Category 1", 10, 19.99);
 
