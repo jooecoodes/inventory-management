852914ab715ff07a525f0272d333eb4159be5aa6
jooecoodes <jefeljoevillacorta@gmail.com>
2025-05-17 19:12:02 +0800

    Configs

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <windows.h>
+#include "VersionHelpers.h"
 
 #include "../include/InventoryApp.hpp"
 
@@ -7,6 +8,16 @@
 int main() {
     SetConsoleOutputCP(CP_UTF8);
 
+    if (!SetConsoleOutputCP(CP_UTF8)) {
+        std::cerr << "Failed to set console output to UTF-8. Error: " << GetLastError() << std::endl;
+        return 1;
+    }
+
+    if (!IsWindows10OrGreater()) {
+        std::cerr << "This application requires Windows 10 or later." << std::endl;
+        return 1;
+    }
+    
     try {
         InventoryApp app;
         app.run();
